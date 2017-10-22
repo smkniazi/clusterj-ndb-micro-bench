809b1e82d27b9f429a96e2e3f50dc5832e85ef7b
Salman Niazi <smkniazi@kth.se>
2017-10-22 13:22:04 +0200

    Auto generated commit msg.

--- src/main/java/se/kth/ndb/test/Worker.java
+++ src/main/java/se/kth/ndb/test/Worker.java
@@ -216,8 +216,8 @@ public class Worker implements Runnable {
       row.setPartitionId(partitionId);
       row.setData(partitionId); // setting the data partition id, used in FTS
       System.out.println(row.getId() + "\t\t" + row.getPartitionId() + "\t\t" + row.getData());
-      //session.makePersistent(row);
-      session.savePersistent(row);
+      session.makePersistent(row);
+//      session.savePersistent(row);
     }
     session.currentTransaction().commit();
     session.close();
