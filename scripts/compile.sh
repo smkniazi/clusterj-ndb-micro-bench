8c295b819d6bc7e0dbc36b5a51c9ac7c460273d0
Salman Niazi <smkniazi@kth.se>
2017-10-23 11:56:27 +0200

    Auto generated commit msg.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,4 +3,5 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/setup
 
-MYSQL_PWD="$password" mysql -uhop -hhadoop36 test -e "select test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$1\" group by threads, test order by numThreads, avgLatency" 
+MYSQL_PWD=$password mysql -u$username -h$dbHost $db -e "select test, numThreads, count(distinct run) as runs, avg(throughput), avg(avgLatency) from ( select run , test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$1\" group by threads, test, run  order by numThreads, avgLatency) as T group by test, numThreads"
+
