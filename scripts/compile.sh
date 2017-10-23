f86f2667c8edb3123a9f6b8d9f52653b2cb57aef
Salman Niazi <smkniazi@kth.se>
2017-10-23 13:12:23 +0200

    Auto generated commit msg.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,5 +3,10 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/setup
 
-MYSQL_PWD=$password mysql -u$username -h$dbHost $db -e "select test, numThreads, count(distinct run) as runs, avg(throughput), avg(avgLatency) from ( select run , test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$1\" group by threads, test, run  order by numThreads, avgLatency) as T group by test, numThreads"
+tests=(PK_ND BATCH_ND PPIS IS FTS)
+prepend=1
+for test in ${tests[@]}
+do
+	MYSQL_PWD=$password mysql -u$username -h$dbHost $db -e "select test, numThreads, count(distinct run) as runs, avg(throughput), avg(avgLatency) from ( select run , test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$test\" group by threads, test, run  order by numThreads, avgLatency) as T group by test, numThreads" > "$test-$prepend.dat"
+done
 
