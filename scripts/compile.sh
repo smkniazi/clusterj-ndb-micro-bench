1064d90b302bc376e0839d7f969984a834424c10
Salman Niazi <smkniazi@kth.se>
2017-10-30 04:58:33 +0100

    Auto generated commit msg.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,6 +6,6 @@ source $DIR/setup
 tests=(PK_ND BATCH_ND PPIS IS FTS)
 for test in ${tests[@]}
 do
-	MYSQL_PWD=$password mysql -u$username -h$dbHost $db -e "select test as \"\#test\", numThreads, count(distinct run) as runs, avg(throughput), avg(avgLatency) from ( select run , test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$test\" and batchSize=$1 group by threads, test, run  order by numThreads, avgLatency) as T group by test, numThreads" > "$test-$1.dat"
+	MYSQL_PWD=$password mysql -u$username -h$dbHost $db -e "select test as \"\#test\", numThreads, count(distinct run) as runs, avg(throughput), avg(avgLatency) from ( select run , test, threads*5 as numThreads, sum(speed) as throughput, avg(latency) as avgLatency from results where test=\"$test\" and batchSize=$1 and updateData=0 group by threads, test, run  order by numThreads, avgLatency) as T group by test, numThreads" > "$test-$1.dat"
 done
 
